cf122d2f7f7e8f8f2e1c1c3c059388b1fd12ccff
J0n4sP <petersen0108@gmail.com>
2022-11-10 14:29:39 +0100

    team form component

---
+++ spa-application/src/components/TeamCreation.vue
@@ -0,0 +1,41 @@
+<script setup>
+import Vue from 'vue'
+const VueFormulate = require('@braid/vue-formulate')
+Vue.use(VueFormulate.default)
+
+
+</script>
+<template>
+    <FormulateForm
+        class="login-form"
+        v-model = "formValues"
+    >
+        <h2 class="form-title">Create</h2>
+        <p>
+            Here it is possible to create a new Team
+        </p>
+        <FormulateInput
+            name="name"
+            type="text"
+            label="Team Name"
+        />
+        <FormulateInput
+            type="submit"
+            label="Create"
+        />
+        <pre
+            class="code"
+            v-text="formValues"
+        />
+    </FormulateForm>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            formValues: {}
+        }
+    },
+}
+</script>
